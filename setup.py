343d01fe739a8906ebda8f0727d470955630541d
Matthew Feickert <matthew.feickert@cern.ch>
2020-08-04 03:53:02 -0500

    Add lint and test extras

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
 from setuptools import setup
 
-extras_require = {
-    "develop": [
-        "check-manifest",
-        "pytest~=6.0",
-        "pytest-cov~=2.8",
-        "pytest-console-scripts~=0.2",
-        "bumpversion~=0.5",
-        "pyflakes",
-        "pre-commit",
-        "black",
-        "twine",
-    ],
-}
+extras_require = {}
+extras_require["lint"] = sorted(set(["pyflakes", "black"]))
+extras_require["test"] = sorted(
+    set(
+        [
+            "check-manifest",
+            "pytest~=6.0",
+            "pytest-cov~=2.8",
+            "pytest-console-scripts~=0.2",
+        ]
+    )
+)
+extras_require["develop"] = sorted(
+    set(
+        extras_require["lint"]
+        + extras_require["test"]
+        + ["bumpversion~=0.5", "pre-commit", "twine"]
+    )
+)
 extras_require["complete"] = sorted(set(sum(extras_require.values(), [])))
 
 setup(extras_require=extras_require)
