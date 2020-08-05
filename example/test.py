3f155fc4e6e06731d264b6cfef7628f3a52d108b
Matthew Feickert <matthew.feickert@cern.ch>
2020-08-05 02:35:12 -0500

    Add improved comments

--- example/test.py
+++ example/test.py
@@ -13,16 +13,16 @@ def main():
     allmatrix2py.initialise(cards_directory_path + "/param_card.dat")
 
     momenta = [
-        [0.5000000e03, 0.0000000e00, 0.0000000e00, 0.5000000e03],
-        [0.5000000e03, 0.0000000e00, 0.0000000e00, -0.5000000e03],
-        [0.5000000e03, 0.1040730e03, 0.4173556e03, -0.1872274e03],
-        [0.5000000e03, -0.1040730e03, -0.4173556e03, 0.1872274e03],
+        [0.5000000e03, 0.0000000e00, 0.0000000e00, 0.5000000e03],  # gluon
+        [0.5000000e03, 0.0000000e00, 0.0000000e00, -0.5000000e03],  # gluon
+        [0.5000000e03, 0.1040730e03, 0.4173556e03, -0.1872274e03],  # t
+        [0.5000000e03, -0.1040730e03, -0.4173556e03, 0.1872274e03],  # t~
     ]
     # fortran/C-python do not order table in the same order
     transposed_momenta = np.array(momenta).T.tolist()
 
-    n_helicity = -1  # means sum over all helicity
-    pdg_ids = [21, 21, 6, -6]  # which pdg ids are requested
+    n_helicity = -1  # -1 means sum over all helicity
+    pdg_ids = [21, 21, 6, -6]  # g, g, t, t~
     scale2 = 0.0  # only used for loop matrix element. should be set to 0 for tree-level
     alphas = 0.13
 
