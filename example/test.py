4b15921cbeb6740cb4cd9f4fc26a41de02816de3
Matthew Feickert <matthew.feickert@cern.ch>
2020-08-05 02:19:30 -0500

    simplify navigation of directory sturcture

--- example/test.py
+++ example/test.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
-import allmatrix2py
+# Generated output directory (ttbar_output) has __init__.py so can import
+import ttbar_output.SubProcesses.allmatrix2py as allmatrix2py
 import numpy as np
+import sys
 
 
 # https://cp3.irmp.ucl.ac.be/projects/madgraph/wiki/FAQ-General-4
 def main():
-    allmatrix2py.initialise("../Cards/param_card.dat")
-    # allmatrix2py.initialise("ttbar_output/Cards/param_card.dat")
+    cmdline_args = sys.argv[1:]
+    cards_directory_path = cmdline_args[0]
+    # Seems to require that in same directory or below Cards/
+    allmatrix2py.initialise(cards_directory_path + "/param_card.dat")
 
     momenta = [
         [0.5000000e03, 0.0000000e00, 0.0000000e00, 0.5000000e03],
@@ -18,14 +22,14 @@ def main():
     transposed_momenta = np.array(momenta).T.tolist()
 
     n_helicity = -1  # means sum over all helicity
-    pdg_ids = [21, 21, 6, -6]  # which pdg is requested
+    pdg_ids = [21, 21, 6, -6]  # which pdg ids are requested
     scale2 = 0.0  # only used for loop matrix element. should be set to 0 for tree-level
     alphas = 0.13
 
-    ans = allmatrix2py.smatrixhel(
+    result = allmatrix2py.smatrixhel(
         pdg_ids, transposed_momenta, alphas, scale2, n_helicity
     )
-    print(f"matrix element is: {ans}")
+    print(f"matrix element is: {result}")
 
 
 if __name__ == "__main__":
--- example/test.sh
+++ example/test.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
-# echo $PWD
+# Generate feynman diagrams from physics processes
 if [[ ! -d ttbar_output ]]; then
   if [[ -f ttbar.mg5 ]]; then
     mg5_aMC ttbar.mg5
   fi
 fi
+# compile FORTRAN and wrap it in Python
 cd ttbar_output/SubProcesses
 if [[ ! -f allmatrix2py.cpython-38-x86_64-linux-gnu.so ]]; then
   make allmatrix2py.so
 fi
-# cd ../../
-# ls -lhtra
-cp ../../test.py .
-# export PYTHONPATH=${PYTHONPATH}:${PWD}/ttbar_output/SubProcesses
-python test.py
+# Navigate to top level of process directory structure (same level as Cards/)
+cd ..
+python ../test.py "Cards"
